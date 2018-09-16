06139f34398b93bda9854e8e7dbb53f45b00f6f6
Gagandeep <32390591+chha0019@users.noreply.github.com>
2018-09-15 20:35:21 -0400

    NEw branch lab2

---
+++ AndroidLabs/app/src/main/java/com/algonquincollege/gagan/androidlabs/ListItemActivity.kt
@@ -0,0 +1,13 @@
+package com.algonquincollege.gagan.androidlabs
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListItemActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_item)
+    }
+}
