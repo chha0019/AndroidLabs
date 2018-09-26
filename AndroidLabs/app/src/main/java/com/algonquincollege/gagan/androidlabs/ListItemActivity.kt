cef4efac1246703ff9abca1269e828cbbb835f71
Gagandeep <32390591+chha0019@users.noreply.github.com>
2018-09-26 11:53:19 -0400

    Updated Lab3

--- AndroidLabs/app/src/main/java/com/algonquincollege/gagan/androidlabs/ListItemActivity.kt
+++ AndroidLabs/app/src/main/java/com/algonquincollege/gagan/androidlabs/ListItemActivity.kt
@@ -1,13 +1,97 @@
 package com.algonquincollege.gagan.androidlabs
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.widget.AlertDialogLayout
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.Switch
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_list_item.*
 
 class ListItemActivity : Activity() {
-
+val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_item)
+        val checkBox = findViewById<CheckBox>(R.id.checkBox)
+        val switch = findViewById<Switch>(R.id.switch1)
+        val imageButton = findViewById<ImageButton>(R.id.imageButton)
+       // val checkBox = findViewById<CheckBox>(R.id.checkBox)
+
+imageButton?.setOnClickListener(View.OnClickListener {
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+        takePictureIntent.resolveActivity(packageManager)?.also {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
     }
+})
+        switch?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+                Toast.makeText(this,isChecked.toString(), Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this,isChecked.toString(), Toast.LENGTH_LONG).show()
+
+            }
+        }
+        checkBox?.setOnCheckedChangeListener { buttonView, isChecked ->
+        if (isChecked)
+        {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage(R.string.dialouge_msg).setTitle(R.string.dialouge_title).setPositiveButton(R.string.OK,{dialog, id ->
+                val resultIntent = Intent( )
+
+                resultIntent.putExtra("Response", "Here is my response")
+
+                setResult(Activity.RESULT_OK, resultIntent)
+
+                finish()
+            }).setNegativeButton(R.string.Cancel,{dialog, id ->
+                val resultIntent = Intent( )
+
+//                resultIntent.putExtra("Response", "Here is my response")
+//
+//                setResult(Activity.RESULT_CANCELED, resultIntent)
+//
+//                finish()
+            }).show()
+        }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data.extras.get("data") as Bitmap
+            imageButton.setImageBitmap(imageBitmap)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
--- AndroidLabs/app/src/main/java/com/algonquincollege/gagan/androidlabs/StartActivity.kt
+++ AndroidLabs/app/src/main/java/com/algonquincollege/gagan/androidlabs/StartActivity.kt
@@ -1,12 +1,68 @@
 package com.algonquincollege.gagan.androidlabs
 
 import android.app.Activity
+import android.app.ListActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class StartActivity : Activity() {
+
+    val activityName = "START_ACTIVITY"
+    val RESULT_CODE =1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        Log.i(activityName, "In onCreate()");
+        val button2 = findViewById<Button>(R.id.button)
+        button2?.setOnClickListener(View.OnClickListener {
+           val anotherActivity = Intent(this, ListItemActivity::class.java)
+            startActivityForResult(anotherActivity,50)
+        })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode,data)
+        if (requestCode==50) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i(activityName, "Returned to ListActivity.onActivityResult")
+                val messagePassed = data?.getStringExtra("Response")
+                Toast.makeText(this,messagePassed,Toast.LENGTH_LONG).show()
+            }
+        }
+        else
+            Log.e(activityName,"Activity not found")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(activityName,"In onResume")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(activityName,"In onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(activityName,"In onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(activityName,"In onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(activityName,"In onDestroy")
+    }
+
 }
